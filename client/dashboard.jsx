45cda61b01d06bc67689249a8a6c042b2b2e77f3
Jessie Tang <jt6432@g.rit.edu>
2024-11-30 22:17:00 -0500

    rid

--- client/dashboard.jsx
+++ client/dashboard.jsx
@@ -4,7 +4,6 @@ const { useState } = React;
 import RecipeForm from './recipeForm.jsx';
 import RecipeList from './recipeList.jsx';
 import AccountDetails from './accountDetails.jsx';
-import RecipeEdit from './recipeEdit.jsx'; 
 import RecipeSearch from './recipeSearch.jsx'; 
 
 const Dashboard = () => {
@@ -20,8 +19,6 @@ const Dashboard = () => {
                 return <RecipeForm triggerReload={() => setReloadRecipes(!reloadRecipes)} />;
             case 'recipeList':
                 return <RecipeList recipes={recipes} reloadRecipes={reloadRecipes} />;
-            case 'recipeEdit':
-                return <RecipeEdit recipeId={1} onRecipeUpdated={() => setReloadRecipes(!reloadRecipes)} />;
             case 'recipeSearch':
                 return <RecipeSearch onSearchResults={setRecipes} />;
             default:
@@ -35,7 +32,6 @@ const Dashboard = () => {
                 <button onClick={() => setActiveComponent('recipeForm')}>Add Recipe</button>
                 <button onClick={() => setActiveComponent('recipeList')}>View Recipes</button>
                 <button onClick={() => setActiveComponent('recipeSearch')}>Search Recipes</button>
-                <button onClick={() => setActiveComponent('recipeEdit')}>Edit Recipe</button>
                 <button onClick={() => setActiveComponent('accountDetails')}>Account Details</button>
             </div>
 
